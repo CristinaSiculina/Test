3f8e96d9ab14c4deedeffc9591af62369cfa3e83
Mystique1987 <siculina_cristina@yahoo.com>
2017-07-30 23:55:42 +0300

    delete

--- Hello.java
+++
@@ -1,7 +0,0 @@
-package com.edocti.javaintro.Hello;
-
-public class Hello {
-	public static void main (String[] args) {
-	System.out.println ("Hello" +args[0] + "!");
-	}
-}
--- Helloworld.java
+++
@@ -1,8 +0,0 @@
-package com.edocti.javaintro.Hello;
-
-public class Helloworld {
-	public static void main (String[] args){
-	System.out.println("Hello World!");
-	}
-}
-	
