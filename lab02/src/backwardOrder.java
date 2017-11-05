4fccfd27eacd92fa55e025b31be404a918077d4c
Cris <30445007+Cristinasiculina@users.noreply.github.com>
2017-11-05 18:10:20 +0200

    HW

---
+++ lab02/src/backwardOrder.java
@@ -0,0 +1,10 @@
+package Test.lab02;
+
+public class backwardOrder {
+	public static void main(String[] args){
+		int[] backnumber = new int[] {1, 2, 3, 4, 5};
+		for (int i = backnumber.length - 1; i >= 0; i--){
+			System.out.print(backnumber[i] + ", ");
+			}
+		}
+}
